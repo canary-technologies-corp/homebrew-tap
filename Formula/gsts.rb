77db287bf6616075fa7dbef283c49e106400d3f4
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-06-25 13:25:30 -0700

    gsts: core Formula patched with ruimarinho/gsts#111

---
+++ Formula/gsts.rb
@@ -0,0 +1,31 @@
+require "language/node"
+
+class Gsts < Formula
+  desc "Obtain and store AWS STS credentials by authenticating via G Suite SAML"
+  homepage "https://github.com/ruimarinho/gsts"
+  url "https://github.com/ruimarinho/gsts/archive/v5.0.4.tar.gz"
+  sha256 "1ba1b5dca60cb59c984a6c49ad8a87e658eec00d296109c5bf5fb2b2e7b810a8"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    (bin/"gsts").write_env_script libexec/"lib/node_modules/gsts/index.js", PLAYWRIGHT_BROWSERS_PATH: "0"
+  end
+
+  def post_install
+    ENV["PLAYWRIGHT_BROWSERS_PATH"]="0"
+
+    system "#{Formula["node"].bin}/node", "#{libexec}/lib/node_modules/gsts/node_modules/playwright/cli.js", "install"
+  end
+
+  patch do
+    url "https://github.com/ruimarinho/gsts/pull/111.patch"
+    sha256 "be8f12ff15e4b34e9823571f3afce7f213aaa5dbbd6a78d4b4a271d9476f29d1"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gsts --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Kalbasit Tap
+
+## How do I install these formulae?
+
+`brew install kalbasit/tap/<formula>`
+
+Or `brew tap kalbasit/tap` and then `brew install <formula>`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "kalbasit/tap"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
